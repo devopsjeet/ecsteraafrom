45a7047ba9ef46a2bd7f150c7c63b2b14886b359
devopsjeet <72682325+devopsjeet@users.noreply.github.com>
2022-02-01 13:26:17 +0530

    Create securitygroup.tf

---
+++ securitygroup.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "firstsg" {
+  name        = "firstsg"
+  description = "Allow TLS inbound traffic"
+
+
+  dynamic "ingress" {
+    for_each = var.ports
+    iterator = port
+    content {
+      description      = "TLS from VPC"
+      from_port        = port.value
+      to_port          = port.value
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+output "sgname" {
+  value = aws_security_group.firstsg.id
+}
