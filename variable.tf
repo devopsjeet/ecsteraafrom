bba897fe48117cfdfd806478e5839e6e1218a6fe
devopsjeet <72682325+devopsjeet@users.noreply.github.com>
2022-02-01 14:38:49 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "ports" {
+  type = list(number)
+
+}
+
+variable "imageid" {
+  type = string
+}
+
+variable "instancetype" {
+  type = string
+}
+
+variable "accesskey" {
+  type = string
+}
+
+variable "secretkey" {
+  type = string
+}
