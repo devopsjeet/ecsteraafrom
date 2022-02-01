8a719d2796529b3853b186a9e842e26b9f36923b
devopsjeet <72682325+devopsjeet@users.noreply.github.com>
2022-02-01 14:41:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIART5JEKPGY23U55OV"
-  secret_key = "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"
+  access_key = var.accesskey
+  secret_key = var.secretkey
 }
+
+
